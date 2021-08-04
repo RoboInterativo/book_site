cc3a23fa82e776ce1c04c6386cc8da85221dad60
chiefexb <chief_exb@mail.ru>
2021-08-05 02:18:07 +0300

    Step 4. If the redirect then throw an exception. If there was an exception, do not download it.
    Шаг 4.
    Если редирект то взводим исключение.
    Если исключение не скачиваем.

--- downloader.py
+++ downloader.py
@@ -4,6 +4,17 @@ from bs4 import BeautifulSoup
 import os
 
 
+def check_for_redirect(base_url,response):
+    print(base_url,response.url)
+
+    if len(response.history)>0:
+        if response.history[0].status_code==302:
+            
+            raise Exception('HTTPError')
+    else:
+       pass
+
+
 def get_book_title (base_url,book_id):
     url=urllib.parse.urljoin(base_url, 'b{}'.format(book_id, allow_fragments=True))
     response=requests.get(url)
@@ -12,14 +23,24 @@ def get_book_title (base_url,book_id):
     book = soup.find(id = 'content').find('h1')
     print (book)
 
+
 def download_book(base_url,book_id):
     url=urllib.parse.urljoin(base_url,'/txt.php?id={}'.format(book_id) )
     response=requests.get(url)
+    print (url)
+    try:
+        check_for_redirect(base_url,response)
+
+    except :
+        print('Redirect')
+    else:
+        f=open('books/b{}.txt'.format(book_id),'wb')
+        f.write(response.content)
+        f.close()
     #print (response.content)
 
-    f=open('books/b{}.txt'.format(book_id),'wb')
-    f.write(response.content)
-    f.close()
+
+
 
 if __name__ == '__main__':
     base_url = 'https://tululu.org'
