4d472e23fe9552b6f6ea54b8342445a9f3d3ea15
chiefexb <chief_exb@mail.ru>
2021-08-05 10:29:41 +0300

    Шаг 5,6,7. Новые фунции get_book_title get_book_author Используeм from pathvalidate import sanitize_filename from transliterate import translit os.path.join
    Если нет исключения то скачиваем файл, в функции скачивания берем заголовок книги в качестве имени файла.
    
    Step 5,6,7.
    New features
    get_book_title
    get_book_author
    Use python modules:
    from pathvalidate import sanitize_filename
    from transliterate import translit
    os.path.join
    
    If there is no exception, then we download the file, in the download function we take the title of the book as the file name.

--- downloader.py
+++ downloader.py
@@ -2,6 +2,8 @@ import urllib.parse
 import requests
 from bs4 import BeautifulSoup
 import os
+from pathvalidate import sanitize_filename
+from transliterate import translit
 
 
 def check_for_redirect(base_url,response):
@@ -9,19 +11,36 @@ def check_for_redirect(base_url,response):
 
     if len(response.history)>0:
         if response.history[0].status_code==302:
-            
+
             raise Exception('HTTPError')
     else:
        pass
 
 
 def get_book_title (base_url,book_id):
-    url=urllib.parse.urljoin(base_url, 'b{}'.format(book_id, allow_fragments=True))
+    url=urllib.parse.urljoin(base_url,'/b{}'.format(book_id) )
     response=requests.get(url)
+    print ('GET BOOK TITLE', response.url)
+    #print (response.text)
+    soup = BeautifulSoup(response.text, 'lxml')
+    book_name_and_author = soup.find(id = 'content').find('h1')
+    book_title = book_name_and_author.get_text().split('::')[0].strip()
+
+    print(response.url)
+    print (book_title)
+    return book_title
+
+
+def get_book_author (response):
+
     print (response)
     soup = BeautifulSoup(response.text, 'lxml')
-    book = soup.find(id = 'content').find('h1')
-    print (book)
+    book_name_and_author = soup.find(id = 'content').find('h1')
+
+    book_author = book_name_and_author.get_text().split('::')[1].strip()
+    print(response.url)
+    print (book_author)
+    return book_author
 
 
 def download_book(base_url,book_id):
@@ -34,7 +53,10 @@ def download_book(base_url,book_id):
     except :
         print('Redirect')
     else:
-        f=open('books/b{}.txt'.format(book_id),'wb')
+        fname='{}.{}.txt'.format(book_id, translit( get_book_title(base_url,book_id) ,'ru',reversed=True) )
+        filename=sanitize_filename(fname)
+        print (filename)
+        f=open(os.path.join('books',filename),'wb')
         f.write(response.content)
         f.close()
     #print (response.content)
